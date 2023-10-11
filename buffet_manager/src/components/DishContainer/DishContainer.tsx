725a9d8c08b053138587cd3054b8e73d1aded5e1
unknown <dreicleal@hotmail.com>
2023-10-11 07:08:27 -0300

    adding frame data to container

--- buffet_manager/src/components/DishContainer/DishContainer.tsx
+++ buffet_manager/src/components/DishContainer/DishContainer.tsx
@@ -1,14 +1,40 @@
+"use client";
 import { useFood } from "@/contexts/foodContext";
 import DishFrame from "./dishFrame/DishFrame";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
+import { useEffect } from "react";
+import { buffetManagerApi } from "@/requests/api";
+
 export default function DishContainer() {
-  const { dishes } = useFood();
-  console.log(dishes)
+  const { dishes, setDishes } = useFood();
+  useEffect(() => {
+    const getDishes = async () => {
+      try {
+        const dishes = await buffetManagerApi.get("/dishes");
+        setDishes(dishes.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getDishes();
+    const interval = setInterval(getDishes, 2500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className={styles.dishContainer}>
       {dishes.length > 0 ? (
         dishes.map((dish, i) => (
-          <DishFrame key={i} name={dish.name} type={dish.type} />
+          <DishFrame
+            key={i}
+            name={dish.name}
+            category={dish.category}
+            extra={dish.extra}
+            level={dish.level}
+            initialTime={0}
+          />
         ))
       ) : (
         <h1>Adicione um prato</h1>
