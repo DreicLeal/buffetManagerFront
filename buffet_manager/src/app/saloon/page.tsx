76e334e7b1f0719b9195c6875c50b667e3473ce3
unknown <dreicleal@hotmail.com>
2023-10-12 07:35:43 -0300

    toggle chat saloon page

--- buffet_manager/src/app/saloon/page.tsx
+++ buffet_manager/src/app/saloon/page.tsx
@@ -1,13 +1,22 @@
+"use client"
 import ChatBox from "@/components/Chat/chatBox";
 import DishContainer from "@/components/DishContainer/DishContainer";
 import Header from "@/components/Header/header";
+import ChatIcon from "@mui/icons-material/Chat";
+import { useState } from "react";
+import styles from "./styles.module.scss";
 
 export default function Saloon() {
+  const [chatToggle, setToggleChat] = useState<boolean>(false);
+  const handleChat = () => {
+    setToggleChat(!chatToggle);
+  };
   return (
     <>
       <Header text="Sala" />
       <DishContainer />;
-      <ChatBox/>
+      <ChatIcon className={styles.toggleChat} onClick={handleChat} />
+      {chatToggle && <ChatBox />}
     </>
   );
 }
