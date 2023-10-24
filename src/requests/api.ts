aeaf32fe9a2a849403ea03504c5f65fa56345586
unknown <dreicleal@hotmail.com>
2023-10-24 14:08:56 -0300

    baseUrl

--- src/requests/api.ts
+++ src/requests/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const buffetManagerApi = axios.create({
-  baseURL: "https://buffetmanagerapi007.onrender.com/",
+  baseURL: "https://buffetmanagerapi007.onrender.com",
   timeout: 8000,
   headers: {
     "Content-Type": "application/json",
