6d23a6a29ea821d70bc340909cd0a27984c64297
unknown <dreicleal@hotmail.com>
2023-10-24 12:39:05 -0300

    fixing database interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -5,8 +5,8 @@ export interface IBuffetDatabase {
   name: string;
   category: string;
   extra?: boolean;
-  level: number;
-  timer: number | null;
+  level?: number;
+  timer?: number | null;
   created_at?: string;
   updated_at?: string;
 }
